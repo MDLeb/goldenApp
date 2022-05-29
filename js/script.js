ea2cdb0363ce1f2becf2149a235a5f34fe75755a
Mariy <m.lebadina@gmail.com>
2022-05-29 21:36:39 +0300

    fixed burger

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-let openMenu = () => {
+const openMenu = () => {  
     document.querySelector(".header__burger").classList.toggle("active");
     document.querySelector(".header").classList.toggle("active");
+
+    document.querySelector(".header__burger").classList.contains("active") ?
+    document.body.style.overflow = 'hidden' : 
+    document.body.style.overflow = 'scroll';
 }
+
+Array.from(document.querySelectorAll('.header__navigation a')).forEach(elem =>
+    elem.addEventListener('click', () => {
+        openMenu()
+    }));
